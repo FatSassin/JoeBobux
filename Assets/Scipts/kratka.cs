b52aa7dfee7f88eb9c8d1f0b16b86591e7c602cd
hubert.talwik@gmail.com <hubert.talwik@gmail.com>
2021-09-23 17:00:59 +0200

    comit

--- Assets/Scipts/kratka.cs
+++ Assets/Scipts/kratka.cs
@@ -2,12 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Wybrane
+{
+    KRZYZYK,
+    KOLKO
+
+}
 public class kratka : MonoBehaviour
 {
+    public Sprite kolko, krzyzyk;
+    Wybrane Wybor = Wybrane.KOLKO; 
+    SpriteRenderer sr;
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
     private void OnMouseDown()
     {
-        Debug.Log("watevah");
+        Debug.Log("Cheesus");
+        sr.sprite = krzyzyk;
+        if (Wybor == Wybrane.KRZYZYK)
+        {
+            sr.sprite = kolko;
+            
+        }
+        else
+        {
+            sr.sprite = krzyzyk;
+        }
     }
+
+    
+    
+
+
+
 
 
 }
