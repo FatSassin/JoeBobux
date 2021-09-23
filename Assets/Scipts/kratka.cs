9a56c08838fbf943ceb15a9896985e66f8e0307f
hubert.talwik@gmail.com <hubert.talwik@gmail.com>
2021-09-23 16:32:24 +0200

    hehe boi

---
+++ Assets/Scipts/kratka.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kratka : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("watevah");
+    }
+
+
+}
